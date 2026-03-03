7d6591a5a79df2b96fd77f374437dcbf2dc9ba33
jihoon <git40@gmail.com>
2026-03-03 15:36:37 +0900

    @ManyToMany 양방향 및 조회 테스트

--- src/main/java/org/example/jpaadvance/entity/User.java
+++ src/main/java/org/example/jpaadvance/entity/User.java
@@ -5,6 +5,9 @@ import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Entity
 @Getter
 @Setter
@@ -14,4 +17,12 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
+
+    @ManyToMany(mappedBy = "userList")
+    private List<Food> foodList = new ArrayList<>();
+
+    public void addFoodList(Food food) {
+        this.foodList.add(food);
+        food.getUserList().add(this); // 외래 키(연관 관계) 설정
+    }
 }
