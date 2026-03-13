91921bb40ac59f8be48a1d45c9990fc5c8ce8598
jihoon <git40@gmail.com>
2026-03-13 11:40:50 +0900

    영속성 전이

--- src/main/java/org/example/jpaadvance/entity/User.java
+++ src/main/java/org/example/jpaadvance/entity/User.java
@@ -4,6 +4,7 @@ package org.example.jpaadvance.entity;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
+import org.hibernate.engine.internal.Cascade;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -18,8 +19,14 @@ public class User {
     private Long id;
     private String name;
 
-    @OneToMany(mappedBy = "user")
+    @OneToMany(mappedBy = "user", cascade = {CascadeType.PERSIST, CascadeType.REMOVE})
     private List<Food> foodList = new ArrayList<>();
 
 
+
+    public void addFoodList(Food food) {
+        this.foodList.add(food);
+        food.setUser(this);  //외래 키 설정
+
+    }
 }
